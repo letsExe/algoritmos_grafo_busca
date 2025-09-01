564334b05bf2a2bcb6c6e75a0ab8fa4425cfa2e5
raquellopes3 <raquelsilveriolopes@gmail.com>
2025-09-01 12:11:23 -0300

    dfs corrigido

--- header/alg_dfs.h
+++ header/alg_dfs.h
@@ -1,8 +1,39 @@
-//
-// Created by Raquel Lopes on 30/08/2025.
-//
+#ifndef DFS_H
+#define DFS_H
 
-#ifndef ALGORITMOS_GRAFO_BUSCA_ALG_DFS_H
-#define ALGORITMOS_GRAFO_BUSCA_ALG_DFS_H
+#include "grafo.h"
+#include <map>
+#include <vector>
+#include <iostream>
 
-#endif //ALGORITMOS_GRAFO_BUSCA_ALG_DFS_H
+using namespace std;
+
+class DFS {
+private:
+    map<string, bool> visitado;   // Marca os vértices visitados
+
+    // Método recursivo auxiliar. Retorna 'true' se encontrou o caminho, 'false' caso contrário.
+    bool dfs_recursivo(
+        const string &vertice,
+        const string &fim,
+        Grafo &grafo,
+        vector<string> &caminho,
+        int &custo_final,
+        int custo_atual,
+        int &iteracao
+    );
+
+public:
+    DFS(); // construtor padrão
+
+    // --- Getters ---
+    map<string, bool> get_visitado();
+
+    // --- Setters ---
+    void set_visitado(map<string, bool> v);
+
+    // --- Executa a DFS e imprime a saída no formato especificado ---
+    void executar(Grafo &grafo, const string &inicio, const string &fim);
+};
+
+#endif
--- src/alg_dfs.cpp
+++ src/alg_dfs.cpp
@@ -1,3 +1,96 @@
-//
-// Created by Raquel Lopes on 30/08/2025.
-//
+#include "../header/alg_dfs.h"
+#include <iostream>
+#include <vector>
+
+// --- Construtor padrão ---
+DFS::DFS() {}
+
+// --- Getters ---
+map<string, bool> DFS::get_visitado() {
+    return visitado;
+}
+
+// --- Setters ---
+void DFS::set_visitado(map<string, bool> v) {
+    this->visitado = v;
+}
+
+// --- Função recursiva auxiliar ---
+bool DFS::dfs_recursivo(
+    const string &vertice,
+    const string &fim,
+    Grafo &grafo,
+    vector<string> &caminho,
+    int &custo_final,
+    int custo_atual,
+    int &iteracao
+) {
+    visitado[vertice] = true;
+    caminho.push_back(vertice);
+
+    // Exibe a iteração atual no formato solicitado
+    cout << "Iteração " << iteracao++ << ":" << endl;
+    // Adaptamos a "Lista" para mostrar o nó atual e a distância acumulada até ele
+    cout << "Lista: (" << vertice << ": dist = " << custo_atual << ")" << endl << endl;
+
+    // Caso base: se o vértice atual é o destino, encontramos o caminho
+    if (vertice == fim) {
+        custo_final = custo_atual;
+        return true;
+    }
+
+    // Itera por todas as arestas para encontrar os vizinhos do vértice atual
+    for (Aresta &aresta : grafo.get_aresta()) {
+        string vizinho;
+        bool aresta_valida = false;
+
+        if (aresta.get_lig_inicio() == vertice) {
+            vizinho = aresta.get_lig_fim();
+            aresta_valida = true;
+        } else if (!grafo.get_orientado() && aresta.get_lig_fim() == vertice) {
+            vizinho = aresta.get_lig_inicio();
+            aresta_valida = true;
+        }
+
+        if (aresta_valida && !visitado[vizinho]) {
+            // Se a chamada recursiva encontrar o 'fim', propaga o sucesso para cima
+            if (dfs_recursivo(vizinho, fim, grafo, caminho, custo_final, custo_atual + aresta.get_custo(), iteracao)) {
+                return true;
+            }
+        }
+    }
+
+    // Backtracking: Se nenhum vizinho levou ao destino, remove o vértice do caminho
+    caminho.pop_back();
+    return false;
+}
+
+// --- Executa a DFS e imprime a saída no formato especificado ---
+void DFS::executar(Grafo &grafo, const string &inicio, const string &fim) {
+    // Inicializações
+    visitado.clear();
+    for (Aresta &aresta : grafo.get_aresta()) {
+        visitado[aresta.get_lig_inicio()] = false;
+        visitado[aresta.get_lig_fim()] = false;
+    }
+    vector<string> caminho;
+    int custo_final = 0;
+    int iteracao = 1;
+
+    cout << "Inicio da execução" << endl;
+
+    bool sucesso = dfs_recursivo(inicio, fim, grafo, caminho, custo_final, 0, iteracao);
+
+    cout << "Fim da execução" << endl;
+    if (sucesso) {
+        cout << "Distância: " << custo_final << endl;
+        cout << "Caminho: ";
+        for (size_t i = 0; i < caminho.size(); ++i) {
+            cout << caminho[i] << (i == caminho.size() - 1 ? "" : " - ");
+        }
+        cout << endl;
+    } else {
+        cout << "Nao foi encontrado um caminho entre '" << inicio << "' e '" << fim << "'." << endl;
+    }
+    cout << endl;
+}
